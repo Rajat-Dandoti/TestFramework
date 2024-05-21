0ef29ccbe1f4c015ca508b7a43527991fe4acedd
Rajat <rajrdandoti@gmail.com>
2024-05-21 22:44:54 +0530

    Docker build file update Workflow update v3 Added .env file

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-docker build -t myregistry/test-python:latest -f docker/Dockerfile.python .
-docker build -t myregistry/test-javascript:latest -f docker/Dockerfile.javascript .
-docker build -t myregistry/test-java:latest -f docker/Dockerfile.java .
+
+set -a
+. ./config/app.env
+set +a
+
+docker build -t $DOCKER_USERNAME/test-python:latest -f docker/Dockerfile.python .
+docker build -t $DOCKER_USERNAME/test-javascript:latest -f docker/Dockerfile.javascript .
+docker build -t $DOCKER_USERNAME/test-java:latest -f docker/Dockerfile.java .
+
+docker push $DOCKER_USERNAME/test-python:latest
+docker push $DOCKER_USERNAME/test-javascript:latest
+docker push $DOCKER_USERNAME/test-java:latest
